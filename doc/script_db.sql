0545fd4108965db8f956244ea7c8455b32cf2dce
arturisimo <arturisimo@gmail.com>
2019-11-29 18:01:00 +0100

    falta schema en el script

--- doc/script_db.sql
+++ doc/script_db.sql
@@ -1,28 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-11-2019 a las 10:20:35
--- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 5.6.34
+CREATE SCHEMA segur_java2 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+
+USE segur_java;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `segur_java`
---
-
--- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `alarmas`
@@ -126,7 +109,7 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Índices para tablas volcadas
+-- ï¿½ndices para tablas volcadas
 --
 
 --
